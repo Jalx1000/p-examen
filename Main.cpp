0ccf4de0098edd56a633ff1710a3caf9021721cd
Javier Mogro <jalejandromogro1@gmail.com>
2023-07-30 22:44:43 -0400

    completando tdas ojo con conjunto y matriz

--- Main.cpp
+++ Main.cpp
@@ -13,22 +13,7 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    VCola2* cola2 = new VCola2;
 
-    int pri = 10;
-    int sec = 20;
-    int ter = 30;
-	int cuar = 40;
-	cola2->poner2(pri);
-	cola2->poner2(sec);
-	cola2->poner2(ter);
-//	cola2->mostrar2();
-	cola2->sacar2(ter);
-//	cola2->mostrar2();
-//	cola2->poner2(cuar);
-	cola2->mostrar2();
-
-//	cout<<cola2->primero2()<<endl;
 
     system("PAUSE");
     return 0;
